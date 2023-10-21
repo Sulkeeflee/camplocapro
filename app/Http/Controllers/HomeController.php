8d04b59a1c2018b4612e6f6434a2955712977ac2
Sulkeeflee <96815455+Sulkeeflee@users.noreply.github.com>
2023-10-21 22:45:57 +0700

    new

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,13 +8,19 @@ use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
 
+use App\Models\Product;
+
+use App\Models\Cart;
+
 
 class HomeController extends Controller
 {
 
     public function index()
     {  
-        return view('home.userpage');
+
+        $product=Product::paginate(3);
+        return view('home.userpage',compact('product'));
     }
         
     public function redirect()
@@ -27,10 +33,92 @@ class HomeController extends Controller
         }
         else
             {
-                return view('home.userpage');
+                $product=Product::paginate(3);
+                return view('home.userpage',compact('product'));
+            }
+    }
+    public function product_details($id)
+    {  
+
+        $product=product:: find ($id) ;
+        return view('home.product_details',compact('product'));          
+    
+    }
+    public function add_cart(Request $request,$id)
+     {
+        if (Auth::id())
+        { 
+
+            $user=Auth::user();
+
+            $product=product::find($id);
+            $cart=new cart;
+            $cart->name=$user->name;
+            $cart->email=$user->email;
+            $cart->phone=$user->phone;
+            $cart->address=$user->address;
+            $cart->user_id=$user->id;
+           
+
+            $cart->Product_title=$product->title;
+
+            if($product->discount!=null)
+
+            {
+                $cart->price=$product->discount * $request->quantity;
             }
 
+            else
+
+            {
+                $cart->price=$product->price * $request->quantity; 
+            }
+
+            $cart->image=$product->image;
+            $cart->Product_id=$product->id;
+            $cart->quantity=$request->quantity;
+
+            $cart->save();
+
+            return redirect()->back();
+
+        }
+        
+        else
+
+        {
+            return redirect('login');
+        }
+        
+    }
+
+    public function show_cart()
+    {
+
+        if(Auth::id())
+
+        {
+            $id=Auth::user()->id; 
+            $cart=cart::where('user_id','=',$id)->get();
+            return view('home.showcart',compact('cart'));
+        }
+
+        else
+        {
+            return redirect('login');
+        }
 
     }
+
+    public function remove_cart($id)
+    {
+        $cart=cart::find($id);
+
+        $cart->delete();
+
+        return redirect()->back();
+    }
+    
+
 }
 
--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,28 @@ Route::middleware([
 Route::get('/redirect', [App\Http\Controllers\HomeController::class, 'redirect']);
 
 Route::get('/view_catagory', [App\Http\Controllers\AdminController::class, 'view_catagory']);
+
+Route::post('/add_catagory', [App\Http\Controllers\AdminController::class, 'add_catagory']);
+
+Route::get('/delete_catagory/{id}', [App\Http\Controllers\AdminController::class, 'delete_catagory']);
+
+Route::get('/view_product', [App\Http\Controllers\AdminController::class, 'view_product']);
+
+Route::post('/add_product', [App\Http\Controllers\AdminController::class, 'add_product']);
+
+Route::get('/show_product', [App\Http\Controllers\AdminController::class, 'show_product']);
+
+Route::get('/delete_product/{id}', [App\Http\Controllers\AdminController::class, 'delete_product']);
+
+Route::get('/update_product/{id}', [App\Http\Controllers\AdminController::class, 'update_product']);
+
+Route::post('/update_product_confirm/{id}', [App\Http\Controllers\AdminController::class, 'update_product_confirm']);
+
+Route::get('/product_details/{id}', [App\Http\Controllers\HomeController::class, 'product_details']);
+
+Route::post('/add_cart/{id}', [App\Http\Controllers\HomeController::class, 'add_cart']);
+
+Route::get('/show_cart', [App\Http\Controllers\HomeController::class, 'show_cart']);
+
+Route::get('/remove_cart/{id}', [App\Http\Controllers\HomeController::class, 'remove_cart']);
+
